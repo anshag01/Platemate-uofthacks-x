794e42aa8305710d08d79afb7fac87fc55a518c0
Matthew Ramsoondar <ramsoondarmat@gmail.com>
2023-01-21 13:29:31 -0500

    feat(home): add user location handling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,28 @@
-import Container from "../components/ui/Container"
-import Header from "../components/ui/Header"
-import Input from "../components/ui/Input"
-import MyComponent from "../components/Map"
+import Container from '../components/ui/Container';
+import Header from '../components/ui/Header';
+import Input from '../components/ui/Input';
+import MyComponent from '../components/Map';
+import { useEffect } from 'react';
+
+import { reverseGeocode } from '../utils/reverseGeocode';
 
 const Home = () => {
-  return (
-    <Container className="flex flex-col gap-y-8">
-      <Header text="Where To?" />
-      <Input type="search" placeholder="Enter a location" />
-      <MyComponent />
-    </Container>
-  )
-}
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            const { longitude, latitude } = position.coords;
+            const formattedAddress = reverseGeocode(longitude, latitude);
+            console.log('formatted address', formattedAddress);
+        });
+    }, []);
 
-export default Home
+    return (
+        <Container className="flex flex-col gap-y-8">
+            <Header text="Where To?" />
+            <Input type="search" placeholder="Enter a location" />
+            <MyComponent />
+        </Container>
+    );
+};
+
+export default Home;
