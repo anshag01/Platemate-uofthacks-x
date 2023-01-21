fc8ca284d574890252433d8fe5c6748c774b276f
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-01-21 02:59:59 -0500

    installed tailwind

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="flex">
+      <ul>
+        <li>Login</li>
+        <li>Logout</li>
+        <li>Signup</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
