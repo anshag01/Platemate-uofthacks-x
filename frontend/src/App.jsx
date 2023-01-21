a6d9685de276e6a817b7ef0bd13aecb3906bb1e6
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-01-21 04:17:49 -0500

    changed where to

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import WhereTo from "./pages/WhereTo"
 
 const App = () => {
-    return (
-        <>
-            {/* <Navbar /> */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/whereTo" element={<WhereTo />} />
-            </Routes>
-        </>
-    );
-};
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/whereTo" element={<WhereTo />} />
+      </Routes>
+    </>
+  )
+}
 
-export default App;
+export default App
