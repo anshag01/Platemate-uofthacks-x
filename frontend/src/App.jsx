2c981587de8710ba81e124c6aa7f00bb20053bf4
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-01-21 04:44:42 -0500

    signup finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 import WhereTo from "./pages/WhereTo"
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/whereTo" element={<WhereTo />} />
       </Routes>
     </>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,16 +4,17 @@ import Button from "../components/ui/Button"
 import Input from "../components/ui/Input"
 import { Link } from "react-router-dom"
 
-const Login = () => {
+const Signup = () => {
   return (
     <Container className="flex flex-col gap-y-8">
-      <Header text="Log In" />
+      <Header text="Sign Up" />
 
       {/* Form */}
+      <Input placeholder="Name" />
       <Input placeholder="Username" />
       <Input placeholder="Password" />
 
-      <Button text="Log In" />
+      <Button text="Sign Up" />
       <Link className="text-[#DF72E1] hover:text-[#a04ea2] hover:ease-in-out duration-100 text-center">
         Forgot your password?
       </Link>
@@ -21,4 +22,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Signup
