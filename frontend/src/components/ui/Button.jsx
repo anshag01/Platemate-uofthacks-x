2f881b0b142e4f24bcc965beb502077377d56c0a
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-01-21 10:32:54 -0500

    Profile finished

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,8 +1,13 @@
-const Button = ({ text, ...props }) => {
+const Button = ({ text, theme = "default", ...props }) => {
   return (
     <button
-      className="h-fit py-2 px-4 rounded-lg font-medium hover:bg-transparent hover:ease-in-out duration-100 
-    shadow-lg text-white hover:text-[#DF72E1] bg-[#DF72E1] border-2 border-[#DF72E1]"
+      className={
+        "h-fit py-2 px-4 rounded-lg font-medium hover:bg-transparent hover:ease-in-out duration-100 shadow-lg text-white " +
+        (theme === "default"
+          ? "hover:text-[#DF72E1] bg-[#DF72E1] border-2 border-[#DF72E1] "
+          : "w-fit hover:text-[#EB4646] bg-[#EB4646] border-2 border-[#EB4646] ") +
+        props.className
+      }
       {...props}
     >
       {text}
--- frontend/src/components/ui/Container.jsx
+++ frontend/src/components/ui/Container.jsx
@@ -1,6 +1,8 @@
 const Container = (props) => {
   return (
-    <div className={"w-full h-full p-8 " + props.className}>
+    <div
+      className={"md:w-[500px] w-full h-full p-8 mx-auto " + props.className}
+    >
       {props.children}
     </div>
   )
