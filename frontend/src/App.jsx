0f3c936f1f01376ba61fcfef0dc35eb714fdf2f2
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-01-21 11:35:01 -0500

    fixed a small bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom"
-import { Wrapper } from "@googlemaps/react-wrapper"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
@@ -7,23 +6,17 @@ import Recommended from "./pages/Recommended"
 import Signup from "./pages/Signup"
 import Profile from "./pages/Profile"
 
-const render = (status) => {
-  console.log("status", status)
-}
-
 const App = () => {
   return (
     <>
       <Navbar />
-      <Wrapper apiKey={process.env.REACT_APP_GOOGLE_API_KEY} render={render}>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/recommended" element={<Recommended />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Wrapper>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/recommended" element={<Recommended />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   )
 }
