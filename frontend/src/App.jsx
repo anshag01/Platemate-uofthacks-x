9d9297ea30e4c65a588d2d8b748cef1190199fea
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-01-21 05:32:41 -0500

    Navbar changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,20 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import Recommended from "./pages/Recommended"
 import Signup from "./pages/Signup"
 import WhereTo from "./pages/WhereTo"
 
 const App = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/whereTo" element={<WhereTo />} />
+        <Route path="/log-in" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/where-to" element={<WhereTo />} />
+        <Route path="/recommended" element={<Recommended />} />
       </Routes>
     </>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
+import { ReactComponent as BlackCalander } from "../assets/icons/blackCalander.svg"
+import { ReactComponent as ColorCalandar } from "../assets/icons/colorCalandar.svg"
+import { ReactComponent as BlackMail } from "../assets/icons/blackMail.svg"
+import { ReactComponent as ColorMail } from "../assets/icons/colorMail.svg"
+import { ReactComponent as BlackPerson } from "../assets/icons/blackPerson.svg"
+import { ReactComponent as ColorPerson } from "../assets/icons/colorPerson.svg"
+
 const Navbar = () => {
   return (
-    <nav className="flex">
-      <ul>
-        <li>Login</li>
-        <li>Logout</li>
-        <li>Signup</li>
-      </ul>
+    <nav
+      className="w-full h-[100px] rounded-t-3xl flex items-center justify-around fixed bottom-0"
+      style={{
+        boxShadow: "0px 20px 100px rgba(0, 0, 0, 0.25)",
+      }}
+    >
+      {/* <BlackCalander /> */}
+      <ColorCalandar />
+      {/* <BlackMail /> */}
+      <ColorMail />
+      {/* <BlackPerson /> */}
+      <ColorPerson />
     </nav>
   )
 }
